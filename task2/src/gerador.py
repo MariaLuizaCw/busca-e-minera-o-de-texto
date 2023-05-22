7ea2edaa06fc5e436b073d372f9006e718ea54ad
Wuillaume Maria <maria.wuillaume@nokia.com>
2023-05-22 15:11:00 -0300

    implement gerador and processador as classes: update with stemmer option

---
+++ task2/src/gerador.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import sys
+from xml.dom import minidom
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+from unidecode import unidecode
+sys.path.insert(1, '../utils')
+from read_config import read_config_file
+from nltk.tokenize import RegexpTokenizer
+
+class gerador():
+    def __init__(self, input_path, results_path) -> None:
+        self.inputs_path = input_path
+        self.results_path = results_path
+        self.config_dict = {}
+        read_config_file(self.inputs_path + 'GLI.CFG', self.config_dict)
+        print('Read Config File')
+        
+    def _parse_xml(self):
+        records_data = []
+        for xml_file in  self.config_dict['LEIA']:
+            parsed_xml = minidom.parse(self.inputs_path + xml_file)
+            records = parsed_xml.getElementsByTagName('RECORD')
+            
+            for record in records:
+                recordnum = record.getElementsByTagName('RECORDNUM')[0].firstChild.nodeValue.strip()
+                abstract = record.getElementsByTagName('ABSTRACT')
+                extract = record.getElementsByTagName('EXTRACT')
+                
+                if len(abstract) > 0:
+                    text = unidecode(abstract[0].firstChild.nodeValue.strip().lower())
+                elif len(extract) > 0:
+                    text = unidecode(extract[0].firstChild.nodeValue.strip().lower())
+                else:
+                    text = ''
+                    
+                records_data.append({'RECORDNUM': recordnum, 'TEXT': text})
+        records_df = pd.DataFrame(records_data)
+        return records_df
+    def genereate_list(self):
+        gli_dict = {}
+        records_df = self._parse_xml()
+        for _, row in records_df.iterrows():
+            text = row['TEXT']
+            recordnum = row['RECORDNUM']
+            tokenizer = RegexpTokenizer(r'\w{3,}')
+            words = tokenizer.tokenize(text)   
+            if self.config_dict['USE'][0] == 'STEMMER':
+                stemmer = PorterStemmer()
+                words = [stemmer.stem(w) for w in words]
+            for w in words:
+                if w in gli_dict:
+                    gli_dict[w].append(recordnum)
+                else:
+                    gli_dict[w] = [recordnum]
+        print('Generete List With ' + self.config_dict['USE'][0])
+        glig_df = pd.DataFrame({'WORDS': gli_dict.keys(), 'RECORDS': gli_dict.values()})
+        glig_df.to_csv(self.results_path+self.config_dict['ESCREVA'][0], index=False, sep=';')
+        print(glig_df)
---
+++ task2/src/processador.py
@@ -0,0 +1,59 @@
+from xml.dom import minidom
+from unidecode import unidecode
+import sys
+import pandas as pd
+sys.path.insert(1, '../utils')
+from read_config import read_config_file
+
+class processador():
+    def __init__(self, input_path, results_path) -> None:
+        self.inputs_path = input_path
+        self.results_path = results_path
+        self.config_dict = {}
+        read_config_file(self.inputs_path + 'PC.CFG', self.config_dict)
+        print('Read Config File')
+    def _parse_xml(self):
+        print('Parsed XML')
+        xml_file = self.inputs_path + self.config_dict['LEIA'][0]
+        parsed_xml = minidom.parse(xml_file)
+        return parsed_xml
+    
+    def _soma_string(self, score):
+        digit_sum = 0
+        for digit in score:
+            digit_sum += int(digit)
+        
+        return digit_sum
+    
+    def generate_consultas(self):
+        xml_data_queries = []
+        query_elements = self._parse_xml().getElementsByTagName('QUERY')
+        for query in query_elements:
+            query_number = query.getElementsByTagName('QueryNumber')[0].firstChild.nodeValue
+            query_text = query.getElementsByTagName('QueryText')[0].firstChild.nodeValue.strip()
+            query_text = unidecode(query_text.replace(';', ' ')).lower()
+            xml_data_queries.append([query_number, query_text])
+
+        df_queries = pd.DataFrame(xml_data_queries, columns=['QueryNumber', 'QueryText'])
+
+        df_queries.to_csv(self.results_path + self.config_dict['CONSULTAS'][0], index=False, sep=';')
+        print('Generate Queries CSV')
+        print(df_queries.head())
+        
+    def generate_esperados(self):
+        xml_data_docs = []
+        parsed_xml = self._parse_xml()
+        for query in parsed_xml.getElementsByTagName('QUERY'):
+            query_number = query.getElementsByTagName('QueryNumber')[0].firstChild.nodeValue
+            for record in query.getElementsByTagName('Records')[0].getElementsByTagName('Item'):
+                doc_number = record.firstChild.nodeValue
+                doc_votes = self._soma_string(record.getAttribute('score'))
+                
+                xml_data_docs.append((query_number, doc_number, doc_votes))
+
+
+        df_docs = pd.DataFrame(xml_data_docs, columns=['QueryNumber', 'DocNumber', 'DocVotes'])
+        df_docs.to_csv(self.results_path + self.config_dict['ESPERADOS'][0], index=False, sep=';')
+        
+        print('Generate Queries CSV')
+        print(df_docs.head())
